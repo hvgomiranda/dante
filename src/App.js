d5e0ea31d6485c939f4010c6fd81e87723c68b13
Hugo Miranda Campos <hugomirandacampos@gmail.com>
2024-04-26 17:23:21 -0300

    commit 0.51

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import VideoDetail from "./components/video-detail/VideoDetail.jsx";
 function App() {
 
   return (
-    <div className="App poppins-regular Scroll-watcher">
+    <div className="App">
       <div className='Left'>
         <Header />
         <Menu />
