99dd7bdf80c7d51e0a755b0f858d053e3df8777b
Hugo Miranda Campos <hugomirandacampos@gmail.com>
2024-05-19 15:31:59 -0300

    Commit 1.29: se agrega un menú responsive

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,25 +1,16 @@
 import "./Menu.css";
 import { Link } from 'react-router-dom';
-import { useState } from "react";
+import { slide as MenuBar} from 'react-burger-menu';
 
 const Menu = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
     return(
-        <nav>
-            <div className="Menu">
-                <button className="Menu-toggle" onClick={toggleMenu}>{menuOpen ? "X" : "Menú"}</button>
-                <ul className={`Menu-list ${menuOpen ? 'Menu-list-open' : ''}`}>
-                    <li><Link to={"/section/film-tv-series"} className="Menu-item">Film / TV series</Link></li>
-                    <li><Link to={"/section/music-videos"} className="Menu-item">Music Videos</Link></li>
-                    <li><Link to={"/section/commercials"} className="Menu-item">Commercials</Link></li>
-                </ul>
-            </div>
-        </nav>
+        <MenuBar right className="Menu">
+            <ul  className="Menu-list">
+                <li><Link to={"/section/film-tv-series"} className="Menu-item">Film / TV series</Link></li>
+                <li><Link to={"/section/music-videos"} className="Menu-item">Music Videos</Link></li>
+                <li><Link to={"/section/commercials"} className="Menu-item">Commercials</Link></li>
+            </ul>
+        </MenuBar>
     );
 };
 
