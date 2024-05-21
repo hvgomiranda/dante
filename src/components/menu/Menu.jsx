ebb926eff87ef6d4d637a1e494f5e98522729369
Hugo Miranda Campos <hugomirandacampos@gmail.com>
2024-05-20 22:20:06 -0300

    Commit 1.37: se corrigen algunos problemas con el menú responsive

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -26,12 +26,11 @@ const DesktopMenu = () => (
     };
 
     return(
-        <MenuBar isOpen={menuOpen} onStateChange={handleStateChange} right className="Menu">
+        <MenuBar isOpen={menuOpen} onStateChange={handleStateChange} right className="Menu" noTransition >
             <ul  className="Menu-list">
                 <li><Link to={"/section/film-tv-series"} className="Menu-item" onClick={closeMenu}>Film / TV series</Link></li>
                 <li><Link to={"/section/music-videos"} className="Menu-item" onClick={closeMenu}>Music Videos</Link></li>
                 <li><Link to={"/section/commercials"} className="Menu-item" onClick={closeMenu}>Commercials</Link></li>
-                <span></span>
                 <li><Link to={"/contact"} className="Menu-item" onClick={closeMenu}>perinidante@gmail.com</Link></li>
             </ul>
         </MenuBar>
